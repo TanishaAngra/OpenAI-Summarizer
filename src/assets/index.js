49fe19cb20a7d1e544023ede1679c45bcdf770b7
TanishaAngra <tanishaangra1511@gmail.com>
2024-02-18 20:21:24 +0530

    new modules

---
+++ src/assets/index.js
@@ -0,0 +1,13 @@
+import link from './link.svg'
+import loader from './loader.svg'
+import copy from './copy.svg'
+import logo from './logo.svg'
+import tick from './tick.svg'
+
+export {
+    link,
+    loader,
+    copy,
+    logo,
+    tick
+}
